be3434923ac91e0ca3a9bd8755a5c30e9119a029
EunsilJo <ucccuccc@gmail.com>
2011-05-22 21:51:54 +0900

    jjo2

---
+++ main2.java
@@ -0,0 +1,16 @@
+package exam.ex.gong;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main2 extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
