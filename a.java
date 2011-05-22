a15d25f31bd05675a741bd2adbef1d7368c29f68
unknown <JJO@.(none)>
2011-05-22 21:44:47 +0900

    jjo

---
+++ a.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+
+
+public class a {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[] = {1,2,3,4,5,6,7,8,9,10};
+		
+		 int i=0;
+		  do {
+			  System.out.println(array[i]%2);
+			  	if((array[i]%2)==0) {
+		   System.out.println(array[i]);
+		   i++;
+		  }
+		  }while(i<10);
+		  
+		  Date d = new Date();
+		  d.get
+
+	}
+
+}
